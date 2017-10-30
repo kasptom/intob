e853802162fc63a6be4246abcbbe15d7792eea74
Tomasz Kasprzyk <kasptom@gmail.com>
2017-10-30 12:11:32 +0100

    Print exemplar entities

--- utils/UjiPencharsParser.py
+++ utils/UjiPencharsParser.py
@@ -23,6 +23,7 @@ class UjiPencharsParser:
                         line = next(iter_file)
                         parts = line.lstrip().split(sep=" ")
                         points_number = int(parts[1])
+                        entity.points_per_stroke[stroke_id] = points_number
                         # print(points_number)
                         # every point consists of two coordinates
                         # points start from the 4th element of the line (3rd index)
@@ -30,9 +31,15 @@ class UjiPencharsParser:
                             point_a = (int(parts[j - 3]), int(parts[j - 2]))
                             point_b = (int(parts[j - 1]), int(parts[j]))
                             entity.increase_vectors_number(stroke_id=stroke_id, point_a=point_a, point_b=point_b)
-                    entities.append(entities)
+                    entities.append(entity)
         return entities
 
 
 penchars = UjiPencharsParser.parse("../data/ujipenchars2.txt")
 print(len(penchars))
+o_entity = [penchar for penchar in penchars if penchar.character_id == 'o'][0]
+z_entity = [penchar for penchar in penchars if penchar.character_id == 'z'][0]
+
+o_entity.print_entity()
+z_entity.print_entity()
+

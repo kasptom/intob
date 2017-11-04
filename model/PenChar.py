e7975b01dc83ff33362fb5e210b8739e7cf93753
Tomasz Kasprzyk <kasptom@users.noreply.github.com>
2017-11-04 17:44:39 +0100

    Equally distant m points (#1)
    * Create path with equally distant sections
    
    * Add debug functions and info
    
    * Add more debug information

--- model/PenChar.py
+++ model/PenChar.py
@@ -1,22 +1,54 @@
 import math
 
 import numpy as np
+from PIL import Image, ImageDraw
+from pip._vendor.distlib.compat import raw_input
 
 from model.Direction import Direction
-from utils.penchars_mapping import mapping
 from utils.penchars_mapping import CLASSES_NUMBER
+from utils.penchars_mapping import mapping
 
 PI = math.pi
-
+M = 16  # number of points in the path
+W, H = 3, 3  # resolution of the rectangle with a character
+MAX_STROKES = 6
+DISPLAY_IF_WARN = False
+RESOLUTION = (3100, 2100)
 
 class PenChar:
-    def __init__(self):
-        self.character_id = ''  # character UTF-8 id
-        self.strokes_number = 0  # number of strokes - max 6
-        self.points_per_stroke = np.array([0] * 6)  # overall points number per stroke
-        self.num_points = np.array([0] * 6)  # number of points for each stroke (max 6 strokes)
-        self.stroke_points = []
-        self.num_directions = np.array([[0] * 8] * 6)  # number of vectors of each type (direction) for each stroke
+    def __init__(self, character_id, strokes_number, stroke_points, unique_identifier=None, debug=False):
+        """
+        :param character_id: e.g. "a"
+        :param strokes_number: number of lines used to draw a character
+        :param stroke_points: list of lists of points for each of the stroke
+        :param unique_identifier: id of the sample (should be unique)
+        """
+        self.character_id = character_id  # character UTF-8 id
+        self.unique_identifier = unique_identifier
+        self.strokes_number = strokes_number  # number of strokes - max 6
+        self.strokes_points = stroke_points
+        self.section_length = self.compute_section_length()
+        self.debug = debug
+
+        self.normalized_path = None
+        self.normalized_path_size = 0
+        self.status = "OK"
+        self.create_normalized_path()
+
+        self.segments_directions = self.compute_directions()
+
+    def compute_section_length(self):
+        path_length = 0
+
+        for stroke_id in range(self.strokes_number):
+            for idx in range(1, len(self.strokes_points[stroke_id])):
+                path_length += PenChar.distance(self.strokes_points[stroke_id][idx - 1],
+                                                self.strokes_points[stroke_id][idx])
+        return path_length / (M - 1)
+
+    @staticmethod
+    def distance(point_a, point_b):
+        return math.sqrt(math.pow(point_a[0] - point_b[0], 2) + math.pow(point_a[1] - point_b[1], 2))
 
     def increase_vectors_number(self, stroke_id, point_a, point_b):
         x, y = PenChar.xy_from_points(point_a, point_b)
@@ -28,16 +60,49 @@ class PenChar:
         print('---')
         print(self.character_id, "strokes: ", self.strokes_number)
         np.set_printoptions(precision=1)
+        points_in_path = 0
         for i in range(self.strokes_number):
-            print("stroke: {0} {1}".format(i, 100 * self.num_directions[i] / self.points_per_stroke[i]))
+            points_in_path += len(self.normalized_path[i])
+            print("original stroke {}: {}".format(i, self.strokes_points[i]))
+            print("normalized stroke {}: {}".format(i, self.normalized_path[i]))
+            # print("stroke: {0} {1}".format(i, 100 * self.num_directions[i] / self.points_per_stroke[i]))
+        print("number of points", points_in_path)
+        print("M=", M)
         print('---')
 
-    def print_stroke_info(self):
-        points_all = 0
-        for count in self.points_per_stroke:
-            points_all += count
-        print("length: {0}, list length: {1}, points: {2}".format(points_all, len(self.stroke_points),
-                                                                  self.stroke_points))
+    def draw_normalized_path(self, title=None):
+        im = Image.new('RGB', (420, 620), (255, 255, 255))
+        draw = ImageDraw.Draw(im)
+        for i in range(self.strokes_number):
+            for j in range(len(self.normalized_path[i]) - 1):
+                a = self.normalized_path[i][j]
+                a = (a[0] / 5, a[1] / 5)
+                b = self.normalized_path[i][j + 1]
+                b = (b[0] / 5, b[1] / 5)
+                draw.line(a + b, fill=255, width=1)
+                PenChar.draw_dot(draw, a)
+                PenChar.draw_dot(draw, b)
+        im.show(title)
+        return im
+
+    def draw_path(self, title=None):
+        im = Image.new('RGB', (420, 620), (255, 255, 255))
+        draw = ImageDraw.Draw(im)
+        for i in range(self.strokes_number):
+            for j in range(len(self.strokes_points[i]) - 1):
+                a = self.strokes_points[i][j]
+                a = (a[0] / 5, a[1] / 5)
+                b = self.strokes_points[i][j + 1]
+                b = (b[0] / 5, b[1] / 5)
+                draw.line(a + b, fill=255, width=1)
+                PenChar.draw_dot(draw, a)
+                PenChar.draw_dot(draw, b)
+        im.show(title)
+        return im
+
+    @staticmethod
+    def draw_dot(draw, xy):
+        draw.ellipse((xy[0] - 2, xy[1] - 2, xy[0] + 2, xy[1] + 2), fill='blue', outline='blue')
 
     def to_vector(self):
         x_vector = np.array([0] * 55)
@@ -71,3 +136,71 @@ class PenChar:
         :return: x (n x 55), y - (n x CLASSES_NUMBER) - where n is the length of the entities list
         """
         return [penchar.to_vector() for penchar in penchars]
+
+    def create_normalized_path(self):
+        normalized_path = [[] for _ in range(self.strokes_number)]
+
+        for i in range(self.strokes_number):
+            stroke = self.strokes_points[i]
+            prev_point = stroke[0]
+            normalized_path[i].append(prev_point)
+            j = 1
+            while j < len(stroke):
+                prev_point = normalized_path[i][-1]
+                point = stroke[j]
+                prev_distance = 0
+                distance = PenChar.distance(prev_point, point)
+
+                k = j
+                while distance < self.section_length and k + 1 < len(stroke):
+                    prev_point = stroke[k]
+                    point = stroke[k + 1]
+                    prev_distance = distance
+                    distance += PenChar.distance(prev_point, point)
+                    k += 1
+
+                utvec = [point[0] - prev_point[0], point[1] - prev_point[1]]
+                norm = PenChar.distance(point, prev_point)
+
+                j = k if k != j else j + 1
+
+                if norm == 0 or j == len(stroke):
+                    continue
+                utvec = [utvec[0] / norm, utvec[1] / norm]
+                path_point = (
+                    round(prev_point[0] + utvec[0] * (self.section_length - prev_distance)),
+                    round(prev_point[1] + utvec[1] * (self.section_length - prev_distance))
+                )
+                normalized_path[i].append(path_point)
+
+        length = 0
+        for i in range(self.strokes_number):
+            length += len(normalized_path[i])
+
+        self.normalized_path_size = length
+
+        if length != M:
+            overall_points = 0
+            for stroke in self.strokes_points:
+                overall_points += len(stroke)
+
+            if self.debug:
+                print("WARN for: {}: normalized path length: {}, M={}, all points count: {}, strokes: {}"
+                      .format(self.unique_identifier, length, M, overall_points, self.strokes_number))
+            self.status = "WARN"
+            if DISPLAY_IF_WARN:
+                path_image = self.draw_path()
+                npath_image = self.draw_normalized_path()
+                raw_input('Press enter to continue: ')
+                path_image.close()
+                npath_image.close()
+        self.normalized_path = normalized_path
+
+    def compute_directions(self):
+        segments_directions = [[0 for _ in range(8)] for _ in range(W * H)]
+        for i in range(self.strokes_number):
+            stroke = self.normalized_path[i]
+            for j in range(len(stroke)):
+                pass
+            # TODO preprocess the image (slant, center of gravity and resolution)
+        return segments_directions
--- utils/UjiPencharsParser.py
+++ utils/UjiPencharsParser.py
@@ -3,9 +3,20 @@ from utils.penchars_mapping import mapping
 
 
 class UjiPencharsParser:
-    @staticmethod
-    def parse(file_name):
+    def __init__(self, debug=False):
+        self.shortest_normalized_path = (16, "")
+        self.longest_normalized_path = (16, "")
+        self.debug = debug
+        self.x_range = [-1, -1]
+        self.y_range = [-1, -1]
+        self.x_range_names = ["sample_id", "sample_id"]
+        self.y_range_names = ["sample_id", "sample_id"]
+        self.train_count = 0
+        self.test_count = 0
+
+    def parse(self, file_name):
         penchars = []
+        problems = 0
 
         with open(file_name, mode="r") as file:
             iter_file = iter(file)
@@ -13,41 +24,87 @@ class UjiPencharsParser:
                 if line.lstrip().startswith("//"):
                     continue
                 elif line.startswith("WORD"):
-                    penchar = PenChar()
                     parts = line.split(sep=" ", maxsplit=3)
-                    penchar.character_id = parts[1]
+                    character_id = parts[1]
+                    sample_id = parts[2].rstrip()
+                    unique_identifier = character_id + "_" + sample_id
 
-                    if penchar.character_id not in mapping:
-                        # print("character: " + penchar.character_id + "not in a mapping")
+                    if character_id not in mapping:
                         continue
+                    if sample_id.startswith("trn"):
+                        self.train_count += 1
+                    if sample_id.startswith("tst"):
+                        self.test_count += 1
 
                     line = next(iter_file)
                     parts = line.lstrip().split(sep=" ", maxsplit=2)
-                    penchar.strokes_number = int(parts[1])
-                    for stroke_id in range(penchar.strokes_number):
+
+                    strokes_number = int(parts[1])
+                    stroke_points = [[] for _ in range(strokes_number)]
+
+                    for stroke_id in range(strokes_number):
                         line = next(iter_file)
                         parts = line.lstrip().split(sep=" ")
                         points_number = int(parts[1])
-                        penchar.points_per_stroke[stroke_id] = points_number
-                        # print(points_number)
                         # every point consists of two coordinates
                         # points start from the 4th element of the line (3rd index)
-                        point_b = None
-                        for j in range(6, points_number * 2 + 3, 2):
-                            point_a = (int(parts[j - 3]), int(parts[j - 2]))
-                            penchar.stroke_points.append(point_a)
-                            point_b = (int(parts[j - 1]), int(parts[j]))
-                            penchar.increase_vectors_number(stroke_id=stroke_id, point_a=point_a, point_b=point_b)
-                        if point_b is not None:
-                            penchar.stroke_points.append(point_b)
+                        for j in range(4, points_number * 2 + 3, 2):
+                            point = (int(parts[j - 1]), int(parts[j]))
+                            self.update_range(point, unique_identifier)
+                            stroke_points[stroke_id].append(point)
+
+                    penchar = PenChar(character_id, strokes_number, stroke_points, unique_identifier, self.debug)
+                    if penchar.status != "OK":
+                        problems += 1
+                        if self.shortest_normalized_path[0] > penchar.normalized_path_size:
+                            self.shortest_normalized_path = (penchar.normalized_path_size, penchar.unique_identifier)
+                        if self.longest_normalized_path[0] < penchar.normalized_path_size:
+                            self.longest_normalized_path = (penchar.normalized_path_size, penchar.unique_identifier)
                     penchars.append(penchar)
-        print("TOTAL SAMPLES: ", len(penchars))
+        print("Total samples: ", len(penchars))
+        print("Problems: ", problems)
+        print("Shortest path: {} {}".format(self.shortest_normalized_path[0], self.shortest_normalized_path[1]))
+        print("Longest path: {}, {}".format(self.longest_normalized_path[0], self.longest_normalized_path[1]))
+        print("x range: {} {}".format(self.x_range, self.x_range_names))
+        print("y range: {} {}".format(self.y_range, self.y_range_names))
+        print("test samples count: ", self.test_count)
+        print("train samples count: ", self.train_count)
         return penchars
 
+    def update_range(self, xy, sample_id):
+        if self.x_range[0] == -1 or self.x_range[0] > xy[0]:
+            self.x_range[0] = xy[0]
+            self.x_range_names[0] = sample_id
+        if self.x_range[1] == -1 or self.x_range[1] < xy[0]:
+            self.x_range[1] = xy[1]
+            self.x_range_names[1] = sample_id
+        if self.y_range[0] == -1 or self.y_range[0] > xy[1]:
+            self.y_range[0] = xy[0]
+            self.y_range_names[0] = sample_id
+        if self.y_range[1] == -1 or self.y_range[1] < xy[1]:
+            self.y_range[1] = xy[1]
+            self.y_range_names[1] = sample_id
 
-# penchars = UjiPencharsParser.parse("../data/ujipenchars2.txt")
+parser = UjiPencharsParser(debug=False)
+penchars = parser.parse("../data/ujipenchars2.txt")
+
 # print(len(penchars))
-# o_entity = [penchar for penchar in penchars if penchar.character_id == 'o'][0]
+x_min_entity = [penchar for penchar in penchars if penchar.unique_identifier == parser.x_range_names[0]][0]
+x_min_entity.draw_normalized_path()
+x_min_entity.draw_path("x_min")
+
+x_max_entity = [penchar for penchar in penchars if penchar.unique_identifier == parser.x_range_names[1]][0]
+x_max_entity.draw_normalized_path()
+x_max_entity.draw_path("x_max")
+
+y_min_entity = [penchar for penchar in penchars if penchar.unique_identifier == parser.y_range_names[0]][0]
+y_min_entity.draw_normalized_path()
+y_min_entity.draw_path("y_min")
+
+y_max_entity = [penchar for penchar in penchars if penchar.unique_identifier == parser.y_range_names[1]][0]
+y_max_entity.draw_normalized_path()
+y_max_entity.draw_path("y_max")
+
 # z_entity = [penchar for penchar in penchars if penchar.character_id == ';'][0]
 # o_entity.print_penchar()
 # z_entity.print_penchar()

859dfa61bea1b3f799b117d641da581bd8e34ad1
Tomasz Kasprzyk <kasptom@gmail.com>
2017-12-04 22:15:44 +0100

    Add non-zero center of mass case

--- utils/tests/test_preprocessor.py
+++ utils/tests/test_preprocessor.py
@@ -17,10 +17,14 @@ class TestPreprocessor(TestCase):
     def test_rotate_strokes(self):
         strokes = [np.array([[-1, 1], [0, 1], [1, 1]])]
         center_of_mass = np.array([0, 0])
+        center_of_mass_2 = np.array([0, 1])
         angle = np.math.pi / 2
 
         expected = [np.array([[-1, -1], [-1, 0], [-1, 1]])]
+        expected_2 = [np.array([[0, 0], [0, 1], [0, 2]])]
 
         result = rotate_strokes(strokes, center_of_mass, angle)
+        result_2 = rotate_strokes(strokes, center_of_mass_2, angle)
 
         np.testing.assert_allclose(expected, result, atol=1e-16)
+        np.testing.assert_allclose(expected_2, result_2, atol=1e-16)
